052ad5768e2b0bb86610a73ea7b29010e1bfcd53
Milosz Lakomy <milosz.lakomy@gmail.com>
2025-11-20 07:45:14 +0100

    Implement two 26-vectors drawn side by side

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,16 @@ const isNonNegativeNumber = (value) => {
     return !isNaN(number) && number >= 0;
 };
 
+const isGeometryEmpty = (geometry) => {
+    return !geometry.attributes.position || geometry.attributes.position.count === 0;
+};
+
+const translationSideBySide = 3.0;
+
 export default function App() {
     const mountRef = React.useRef(null);
     const [hull, setHull] = React.useState(null);
+    const [hull2, setHull2] = React.useState(null);
     const [textareaPlaceholder, setTextareaPlaceholder] = React.useState(
         "E.g.: " +
         "[6,5,6, 5,3,5, 6,5,6, " +
@@ -20,6 +27,8 @@ export default function App() {
         " 6,5,6, 5,3,5, 6,5,6]"
     );
     const [textareaValue, setTextareaValue] = React.useState(textareaPlaceholder);
+    const [textarea2Placeholder, setTextarea2Placeholder] = React.useState("");
+    const [textarea2Value, setTextarea2Value] = React.useState(textarea2Placeholder);
 
     React.useEffect(() => {
         // const canvasWidth = window.innerWidth;
@@ -75,6 +84,9 @@ export default function App() {
                 // Update rotation based on mouse movement
                 newHull.rotation.y += deltaMove.x * 0.01;
                 newHull.rotation.x += deltaMove.y * 0.01;
+
+                newHull2.rotation.y += deltaMove.x * 0.01;
+                newHull2.rotation.x += deltaMove.y * 0.01;
             }
 
             previousMousePosition = {
@@ -112,16 +124,30 @@ export default function App() {
         };
 
 
+        const scale = Math.min(canvasWidth, canvasHeight) / 100 / 10 / 1.5;
+
         // Create initial hull
-        const hullGeometry = getHullGeometry("");
+        const hullGeometry = getHullGeometry("", textareaPlaceholder);
         const hullMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff, side: THREE.DoubleSide });
         const newHull = new THREE.Mesh(hullGeometry, hullMaterial);
-        const scale = Math.min(canvasWidth, canvasHeight) / 100 / 10;
         newHull.scale.set(scale, scale, scale);
+        // newHull.position.x = -translationSideBySide;
         // newHull.rotation.y = 0.075;  // Radians
         // newHull.rotation.x = 0.100;  // Radians
         scene.add(newHull);
         setHull(newHull);
+
+
+        // Create initial hull2
+        const hull2Geometry = getHullGeometry("", textarea2Placeholder);
+        const hull2Material = new THREE.MeshStandardMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+        const newHull2 = new THREE.Mesh(hull2Geometry, hull2Material);
+        newHull2.scale.set(scale, scale, scale);
+        newHull2.position.x = translationSideBySide;
+        // newHull2.rotation.y = 0.075;  // Radians
+        // newHull2.rotation.x = 0.100;  // Radians
+        scene.add(newHull2);
+        setHull2(newHull2);
 
 
         animate();
@@ -134,7 +160,8 @@ export default function App() {
             mountRef.current.removeEventListener("mousemove",  onMouseMove);
             mountRef.current.removeEventListener("wheel",      onMouseWheel);
             mountRef.current.removeChild(renderer.domElement);
-            scene.remove(newHull);
+            if (newHull)  { scene.remove(newHull);  }
+            if (newHull2) { scene.remove(newHull2); }
         };
     }, []);
 
@@ -144,13 +171,27 @@ export default function App() {
         setTextareaValue(newTextareaValue);
         updateHullGeometry(newTextareaValue);
     };
+    const handleTextarea2ValueChange = (e) => {
+        const newTextarea2Value = e.target.value;
+        setTextarea2Value(newTextarea2Value);
+        updateHull2Geometry(newTextarea2Value);
+    }
     const updateHullGeometry = (newTextareaValue) => {
         if (!hull) { return; }
-        const geometry = getHullGeometry(newTextareaValue);
+        const geometry = getHullGeometry(newTextareaValue, textareaPlaceholder);
         hull.geometry.dispose();  // Cleanup old geometry
         hull.geometry = geometry; // Set new geometry
     };
-    const getHullGeometry = (newTextareaValue) => {
+    const updateHull2Geometry = (newTextarea2Value) => {
+        if (!hull2) { return; }
+        const geometry = getHullGeometry(newTextarea2Value, textarea2Placeholder);
+        hull2.geometry.dispose();  // Cleanup old geometry
+        hull2.geometry = geometry; // Set new geometry
+
+        if (isGeometryEmpty(geometry)) { hull.position.x =  0; }
+        else { hull.position.x = -translationSideBySide; }
+    };
+    const getHullGeometry = (newTextareaValue, defaultTextareaValue) => {
         const vc = new VertexCloud({ side_length: 10, vertices_per_side: 11 });
         const ac = [-1, 0, 1];  // Allowed coordinates
         const vectors = [];
@@ -159,8 +200,9 @@ export default function App() {
         }}}
         let matches = newTextareaValue.match(/-?\d+\.?\d*/g);
         if (matches === null) {
-            matches = textareaPlaceholder.match(/-?\d+\.?\d*/g);
+            matches = defaultTextareaValue.match(/-?\d+\.?\d*/g);
         }
+        if (matches === null) { return new THREE.BufferGeometry(); /* Empty geometry */ }
         const v26 = matches.map(Number);
         for (let i = 0; i < v26.length; i++) {
             vc.deleteBehindPlane({ plane: new Plane({ normal_versor: new Versor({ x: vectors[i][0], y: vectors[i][1], z: vectors[i][2] }), distance_from_origin: v26[i] / (vectors[i][0]**2 + vectors[i][1]**2 + vectors[i][2]**2)**0.5 }) });
@@ -185,6 +227,20 @@ export default function App() {
 
                     value={textareaValue}
                     onChange={handleTextareaValueChange}
+                />
+                <br />
+                <br />
+                Paste another 26-vector:
+                <br />
+                <textarea
+                    style={{
+                        width: "32vw",
+                        height: "6em",
+                        resize: "vertical", /* Disallow horizontal resizing */
+                    }}
+
+                    value={textarea2Value}
+                    onChange={handleTextarea2ValueChange}
                 />
                 <br />
                 <br />
